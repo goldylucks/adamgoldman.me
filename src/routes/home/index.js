9c4b3c6a40151a780d0928073b9aec44ffcd6dfc
goldylucks <adamgoldman7@gmail.com>
2017-10-23 14:15:36 +0200

    fix home layout

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import Home from './Home';
-import Layout from '../../components/Layout';
 
 function action() {
   return {
     chunks: ['home'],
     title: 'Adam Goldman',
-    component: (
-      <Layout>
-        <Home />
-      </Layout>
-    ),
+    component: <Home />,
   };
 }
 
