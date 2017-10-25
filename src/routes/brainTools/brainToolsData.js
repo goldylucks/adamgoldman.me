01899d705e88dcffb4eda11c79ad8de683ffb770
goldylucks <adamgoldman7@gmail.com>
2017-10-25 14:39:02 +0200

    sort posts and tools

--- src/routes/brainTools/brainToolsData.js
+++ src/routes/brainTools/brainToolsData.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
   export default [
   {
+    "title": "Smoking Destroyer",
+    "description": "Convert your old smoking habit into a powerful image of yourself",
+    "url": "smoking-destroyer"
+  },
+  {
     "title": "Feel Good Generator",
     "description": "Intensify good feelings on command. How much pleasure can you stand?",
     "url": "feel-good-generator"
@@ -9,22 +14,6 @@
     "title": "Grief To Appreciation",
     "description": "Turn feelings of loss, miss, and grief into appreciation, presence and warmth",
     "url": "grief-to-appreciation"
-  },
-  {
-    "title": "Internal Dialog Scrambeler",
-    "description": "Stop recurring intrusive thoughts and limiting beliefs. Perfect for \"what if's, \"I'm not good enough\", and all that lovely things we said to ourselves",
-    "url": "internal-dialog-scrambeler"
-  },
-  {
-    "title": "Loved Ones Amplifier (Generalizer)",
-    "description": "Leverage what you've learned in the loved one amplifier",
-    "IS_DRAFT": true,
-    "url": "loved-one-amplifier-generalizer"
-  },
-  {
-    "title": "Loved Ones Amplifier",
-    "description": "Enjoy people you like like never before",
-    "url": "loved-one-amplifier"
   },
   {
     "title": "Nail Biting Destroyer",
@@ -37,9 +26,19 @@
     "url": "perfect-day"
   },
   {
-    "title": "Recurring Time Distortion",
-    "description": "Turn bad time to good time and good time to wonderful time by distorting time subjectively",
-    "url": "recurring-time-distortion"
+    "title": "Trauma Relief",
+    "description": "Turn old traumas and bad memories, arguments, and fights into powerful resources",
+    "url": "trauma-relief"
+  },
+  {
+    "title": "Internal Dialog Scrambeler",
+    "description": "Stop recurring intrusive thoughts and limiting beliefs. Perfect for \"what if's, \"I'm not good enough\", and all that lovely things we said to ourselves",
+    "url": "internal-dialog-scrambeler"
+  },
+  {
+    "title": "Loved Ones Amplifier",
+    "description": "Enjoy people you like like never before",
+    "url": "loved-one-amplifier"
   },
   {
     "title": "Reverse Feeling Spin",
@@ -52,13 +51,8 @@
     "url": "reverse-feeling-spin2"
   },
   {
-    "title": "Smoking Destroyer",
-    "description": "Convert your old smoking habit into a powerful image of yourself",
-    "url": "smoking-destroyer"
-  },
-  {
-    "title": "Trauma Relief",
-    "description": "Turn old traumas and bad memories, arguments, and fights into powerful resources",
-    "url": "trauma-relief"
+    "title": "Recurring Time Distortion",
+    "description": "Turn bad time to good time and good time to wonderful time by distorting time subjectively",
+    "url": "recurring-time-distortion"
   }
 ]
--- tools/generateBrainTools.js
+++ tools/generateBrainTools.js
@@ -13,10 +13,21 @@ const pathToWriteFile = path.resolve(
   'brainToolsData.js',
 );
 const dirToReadPath = path.resolve(__dirname, '..', 'src', 'brainTools');
-const fileToWrite = fs
-  .readdirSync(dirToReadPath)
-  .filter(onlyJs)
-  .map(fileNameToObject);
+
+// tools by rendering order
+const fileToWrite = [
+  'smoking-destroyer',
+  'feel-good-generator',
+  'grief-to-appreciation',
+  'nail-biting-destroyer',
+  'perfect-day',
+  'trauma-relief',
+  'internal-dialog-scrambeler',
+  'loved-one-amplifier',
+  'reverse-feeling-spin',
+  'reverse-feeling-spin2',
+  'recurring-time-distortion',
+].map(fileNameToObject);
 
 // write file brainTools.js
 fs.writeFileSync(
@@ -24,16 +35,12 @@ fs.writeFileSync(
   `/* eslint-disable */
   export default ${JSON.stringify(fileToWrite, null, 2)}`,
 );
+
 function fileNameToObject(fileName) {
   // eslint-disable-next-line global-require,import/no-dynamic-require
-  const { ...tool } = require(path.resolve(dirToReadPath, fileName))
-  delete tool.default
-  delete tool.stepCount
-  delete tool.nick
+  const { ...tool } = require(path.resolve(dirToReadPath, fileName));
+  delete tool.default;
+  delete tool.stepCount;
+  delete tool.nick;
   return { ...tool, url: fileName.split('.js')[0] };
-}
-
-// Filter css files
-function onlyJs(fileName) {
-  return fileName.includes('.js');
 }
