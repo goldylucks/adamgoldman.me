ab2e77e156245f87ad47a4f2cb05b741b3635ea7
goldylucks <adamgoldman7@gmail.com>
2017-12-12 16:18:56 +0100

    update welcome aboard page

--- src/pages/welcome-aboard.js
+++ src/pages/welcome-aboard.js
@@ -1,6 +1,6 @@
 export default {
   title: 'Welcome Aboard!',
-  nick: 'welcome aboard',
+  nick: 'all aboard!',
   description: 'Just a warm welcome to the inner circle',
   tags: [],
   body: `
@@ -12,5 +12,11 @@ Smart move my friend ;)
 **Expect me to knock on your virtual door** every once in a while, **cool stuff guaranteed**. In the meantime, anything else [you want to say](/lets-talk/)?
 
 btw, one of my virtual minions might have sent you a quick email to make sure we got the address right, and that you want to get more from [me](/who-am-i-anyway/).
+
+In the meantime, feel free to [play with some of the tools](/tools/) and learn from some [case studies](/successes/).
+
+As you may know all my content is, and forever will be, 100% [free](/i-dont-charge-i-accept/) and [open sourced](/open-source/), so don’t go opening your wallet just yet, unless you wish to [donate](/i-dont-charge-i-accept/), which is always appreciated, tho not necessary for you to enjoy my content :)
+
+Let's keep talking and make it a great day!
 `,
 }
