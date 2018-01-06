a72a8daeea7e0ee411269c42d2b7a36e29e03476
goldylucks <adamgoldman7@gmail.com>
2018-01-06 20:12:21 +0700

    content

--- src/routes/loss/Loss.js
+++ src/routes/loss/Loss.js
@@ -15,7 +15,7 @@ const Loss = ({ sections, title }: Props) => (
     <div className="container">
       <div className="mainheading">
         <h1 className="sitetitle">{title}</h1>
-        <p className="lead">Proven <Link to="/loss/protocol">protocol</Link> to experience a more peacful, <Link to="/loss/resourceful-response">resource response</Link> to loss</p>
+        <p className="lead">Proven <Link to="/loss/protocol">protocol</Link> to experience a more peacful, <Link to="/loss/resourceful-response">resourceful response</Link> to loss</p>
       </div>
       <section>
         <div className="card-columns card-columns-three listrecent">
