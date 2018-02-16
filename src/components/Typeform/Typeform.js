153d4f8c8181e6f1becf81ca636d7ea8d35266ab
Dennis S <dbsolde@gmail.com>
2018-02-17 06:45:43 +0800

    mounting component

--- src/components/Typeform/Typeform.js
+++ src/components/Typeform/Typeform.js
@@ -7,16 +7,18 @@ type Props = {
 }
 
 class Typeform extends React.Component {
+  state = {
+    isMounted: false,
+  }
+
   componentDidMount() {
     /* eslint-disable */
     let qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = 'typef_orm', b = 'https://embed.typeform.com/';
     if (!gi.call(d, id)) {
       js = ce.call(d, 'script'); js.id = id; js.src = `${b}embed.js`; q = gt.call(d, 'script')[0]; q.parentNode.insertBefore(js, q)
     } else {
-      typeformEmbed.makeWidget(this.el, this.props['data-url'], {
-        hideHeaders: true,
-        hideFooter: true,
-      })
+      this.typeFormEbedd(this.props['data-url'])
+      this.setState({ isMounted: true })
     }
     /* eslint-enable */
     window.addEventListener('message', this.onWindowMessage)
@@ -27,13 +29,17 @@ class Typeform extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps['data-url'] !== this.props['data-url']) {
-      /* eslint-disable */
-      typeformEmbed.makeWidget(this.el, this.props['data-url'], {
-        hideHeaders: true,
-        hideFooter: true,
-      })
+    if (nextProps['data-url'] !== this.props['data-url'] && this.state.isMounted) {
+      this.typeFormEbedd(nextProps['data-url'])
     }
+  }
+
+  typeFormEbedd = (dataUrl) => {
+    /* eslint-disable */
+    typeformEmbed.makeWidget(this.el, dataUrl, {
+      hideHeaders: true,
+      hideFooter: true,
+    })
   }
 
   props: Props
