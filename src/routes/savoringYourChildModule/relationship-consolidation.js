2b2e38c394afbf868253bc2ac208699bbc8ed353
goldylucks <adamgoldman7@gmail.com>
2018-02-18 00:54:38 +0700

    add testimonials to savoring modules

--- src/routes/savoringYourChildModule/relationship-consolidation.js
+++ src/routes/savoringYourChildModule/relationship-consolidation.js
@@ -2,6 +2,35 @@ export default {
   title: 'Savoring The Future',
   description: 'Savor the future plans & dreams for your child',
   typeform: 'https://adamgoldman.typeform.com/to/lDT9YI',
-  testimonials: [],
+  testimonials: [
+    {
+      title: 'My heart swells with love',
+      text: 'I can think about him and feel happy. I have been going through positive memories and they make me so happy and I feel so much love. My heart swells with love. you are awesome',
+      name: 'Elizabeth West',
+      nameMeta: 'Mother of Tyler, died of suicide',
+      imgSrc: '',
+    },
+    {
+      title: 'Grateful for all the memories',
+      text: 'I liked looking back at the many experiences I had with Shayna, thinking how many great times we had. How grateful for all the memories.',
+      name: 'Tywana M. Smith',
+      nameMeta: 'Mother of Shayna, died unexpectedly in her sleep',
+      imgSrc: '',
+    },
+    {
+      title: 'Can’t thank you enough',
+      text: 'I never knew how much I missed having this experience! I am so appreciative Adam I can’t thank you enough from the bottom of my heart!',
+      name: 'S.Z.',
+      nameMeta: 'Mother of G, died after 1.5 years in the hospital',
+      imgSrc: '',
+    },
+    {
+      title: 'Wonderful memories I forgot!',
+      text: 'I have to process all of this … I did have some wonderful memories Aymen and I shared that I even forgot about! OK thank you so much Adam - I am going to be thinking about this, thank you!',
+      name: 'Tracy Soussi',
+      nameMeta: 'Mother of Aymen, died from illness',
+      imgSrc: '',
+    },
+  ],
   faq: [],
 }
