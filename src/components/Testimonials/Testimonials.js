6a66fa02d26e6ae4261be93a5f9cbe48779e4143
Dennis S <dbsolde@gmail.com>
2018-02-22 16:05:55 +0800

    Testimonial slider centered

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -12,14 +12,13 @@ type Props = {
 }
 
 const Testimonials = ({ testimonials }: Props) => {
-  const slides = (testimonials.length < 3) ? 2 : 3
   const settings = {
     dots: false,
     infinite: false,
     speed: 500,
     arrows: testimonials.length > 3,
     slidesToScroll: 1,
-    slidesToShow: slides,
+    slidesToShow: (testimonials.length < 3) ? 2 : 3,
     responsive: [
       {
         breakpoint: 1024,
@@ -31,22 +30,24 @@ const Testimonials = ({ testimonials }: Props) => {
         breakpoint: 990,
         settings: {
           slidesToShow: 2,
+          arrows: testimonials.length > 2,
         },
       },
       {
         breakpoint: 764,
         settings: {
           slidesToShow: 1,
+          arrows: testimonials.length > 1,
         },
       },
     ],
   }
-
+  const currentSlide = testimonials.length
   return (
-    <div className={`container ${s.testimonial} `}>
-      <Slider {...settings}>
+    <div className="container">
+      <Slider {...settings} className={`countSlide-${currentSlide}`}>
         {testimonials.map(item => (
-          <div>
+          <div className={`totalSlides-${testimonials.length}-${testimonials.length}`}>
             <TestimonialItem item={item} />
           </div>
         ))}
