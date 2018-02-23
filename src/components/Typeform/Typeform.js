27cbaa179d9b1d0df9438867ee014b4b52963b68
goldylucks <adamgoldman7@gmail.com>
2018-02-24 00:23:58 +0700

    Typeform fix

--- src/components/Typeform/Typeform.js
+++ src/components/Typeform/Typeform.js
@@ -7,31 +7,27 @@ type Props = {
 }
 
 class Typeform extends React.Component {
-  state = {
-    isMounted: false,
-  }
-
   componentDidMount() {
     /* eslint-disable */
     let qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = 'typef_orm', b = 'https://embed.typeform.com/';
     if (!gi.call(d, id)) {
       js = ce.call(d, 'script'); js.id = id; js.src = `${b}embed.js`; q = gt.call(d, 'script')[0]; q.parentNode.insertBefore(js, q)
     } else {
-      this.typeFormEbedd(this.props['data-url'])
-      this.setState({ isMounted: true })
+      this.typeFormEmbedd(this.props['data-url'])
     }
     /* eslint-enable */
     window.addEventListener('message', this.onWindowMessage)
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps['data-url'] !== this.props['data-url'] && this.state.isMounted) {
-      this.typeFormEbedd(nextProps['data-url'])
+    if (nextProps['data-url'] !== this.props['data-url']) {
+      this.typeFormEmbedd(nextProps['data-url'])
     }
   }
 
   componentWillUnmount() {
     window.removeEventListener('message', this.onWindowMessage)
+    window.clearTimeout(this.typeformEmbedTimeout)
   }
 
   props: Props
@@ -56,8 +52,14 @@ class Typeform extends React.Component {
     this.props.onSubmit()
   }
 
-  typeFormEbedd = (dataUrl) => {
-    this.typeformEmbed.makeWidget(this.el, dataUrl, {
+  typeFormEmbedd = (dataUrl) => {
+    if (!global.typeformEmbed) {
+      this.typeformEmbedTimeout = setTimeout(() => {
+        this.typeFormEmbedd(dataUrl)
+      }, 500)
+      return
+    }
+    global.typeformEmbed.makeWidget(this.el, dataUrl, {
       hideHeaders: true,
       hideFooter: true,
     })
