f5f39a6ede5bb650e3d4f2a28795545e286953ff
Dennis S <dbsolde@gmail.com>
2018-02-26 12:35:29 +0800

    Module completed by user

--- src/components/SelectedCard/SelectedCard.js
+++ src/components/SelectedCard/SelectedCard.js
@@ -16,10 +16,12 @@ type Props = {
   description: string,
   createdAt: string,
   tags?: [],
+  user: Object,
+  formId: string,
 }
 
 const SelectedCard = ({
-  url, title, imgSrc, description, createdAt, tags,
+  url, title, imgSrc, description, createdAt, tags, formId, user,
 }: Props) => (
   <article className="card">
     {imgSrc && (
@@ -27,7 +29,7 @@ const SelectedCard = ({
         <img className="card-img-top" src={imgSrc} alt={title} />
       </Link>
     )}
-    <div className={`card-body ${s.selectedCardBorder}`}>
+    <div className={`card-body ${user.form && user.form.includes(formId) && s.selectedCardBorder}`} >
       <h2 className="card-title"><Link to={url}>{title}</Link></h2>
       <h4 className="card-text">{description}</h4>
       <Tags tags={tags} />
@@ -38,7 +40,11 @@ const SelectedCard = ({
           </span>
           <span className="post-read-more">
             <Link to={url} title="Read Story">
-              <FA name="check" style={{ fontSize: '25px', color: '#00C5FF' }} />
+              {user.form && (user.form.includes(formId)) ?
+                <FA name="check" style={{ fontSize: '25px', color: '#00C5FF' }} />
+                :
+                <svg className="svgIcon-use" width="25" height="25" viewBox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fillRule="evenodd" /></svg>
+              }
             </Link>
           </span>
         </div>
--- src/routes/savoringYourChildModules/savoringYourChildModules.js
+++ src/routes/savoringYourChildModules/savoringYourChildModules.js
@@ -6,39 +6,54 @@ import MessageMe from '../../components/MessageMe'
 import SelectedCard from '../../components/SelectedCard'
 
 const modules = [
-  { title: 'Peaceful Ending', description: 'Attach the moment of transition to your last happy memory', url: 'peaceful-ending' },
-  { title: 'Savoring The Future', description: 'Savor the future plans & dreams for your child', url: 'savoring-the-future' },
-  { title: 'Reunion', description: 'Reunite with your child\'s presence and valued relationship', url: 'reunion' },
-  { title: 'Reengaging The Future', description: 'Let the qualities of your child propel you towards a brightening future', url: 'reengaging-the-future' },
-  { title: 'Special Days', description: 'Birthdays, holidays, and transition days as an opportunity', url: 'special-days' },
-  { title: 'Relationship Consolidation', description: 'Soften bad experiences & intensify positive ones', url: 'relationship-consolidation' },
+  {
+    title: 'Peaceful Ending', description: 'Attach the moment of transition to your last happy memory', url: 'peaceful-ending', formId: 'Rwa8iu',
+  },
+  {
+    title: 'Savoring The Future', description: 'Savor the future plans & dreams for your child', url: 'savoring-the-future', formId: 'UGn1TQ',
+  },
+  {
+    title: 'Reunion', description: 'Reunite with your child\'s presence and valued relationship', url: 'reunion', formId: 'hcBBCM',
+  },
+  {
+    title: 'Reengaging The Future', description: 'Let the qualities of your child propel you towards a brightening future', url: 'reengaging-the-future', formId: 'A3Pjm8',
+  },
+  {
+    title: 'Special Days', description: 'Birthdays, holidays, and transition days as an opportunity', url: 'special-days', formId: 'kERZFQ',
+  },
+  {
+    title: 'Relationship Consolidation', description: 'Soften bad experiences & intensify positive ones', url: 'relationship-consolidation', formId: 'lDT9YI',
+  },
+  {
+    title: 'Test 1', description: 'Soften bad experiences & intensify positive ones', url: 'test1', formId: 'FYg9Da',
+  },
+  {
+    title: 'Test 2', description: 'Soften bad experiences & intensify positive ones', url: 'test2', formId: 'm4iegB',
+  },
 ]
 
 type Props = {
   user: Object
 }
 
-const SavoringYourChildModules = ({ user }:Props) => {
-  global.console.log('user', user)
-  return (
-    <div>
-      <div className="container">
-        <div className="mainheading">
-          <h1 className="sitetitle">Which module you want to explore next?</h1>
-        </div>
-        <section className="recent-posts">
-          <div className="card-columns listrecent">
-            {modules
-              .map(m => (
-                <SelectedCard {...m} url={`/savoring-your-child/${m.url}`} key={m.url} />
-              ))}
-          </div>
-        </section>
-        <hr />
-        <MessageMe />
+const SavoringYourChildModules = ({ user }:Props) => (
+  <div>
+    <div className="container">
+      <div className="mainheading">
+        <h1 className="sitetitle">Which module you want to explore next?</h1>
       </div>
+      <section className="recent-posts">
+        <div className="card-columns listrecent">
+          {modules
+              .map(m => (
+                <SelectedCard {...m} url={`/savoring-your-child/${m.url}`} key={m.url} user={user} />
+              ))}
+        </div>
+      </section>
+      <hr />
+      <MessageMe />
     </div>
-  )
-}
+  </div>
+)
 
 export default SavoringYourChildModules
