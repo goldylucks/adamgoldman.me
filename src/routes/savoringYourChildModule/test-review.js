1e6201e93f63c60cb8dc8a57d5e7ad3a749bd00b
goldylucks <adamgoldman7@gmail.com>
2018-02-26 13:44:41 +0700

    (Savoring): add test review

---
+++ src/routes/savoringYourChildModule/test-review.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'Test Review',
+  description: 'Test review, feedback, and payments',
+  typeform: 'https://adamgoldman.typeform.com/to/ubvp4J',
+  benefits: [],
+  testimonials: [],
+  faq: [],
+}
