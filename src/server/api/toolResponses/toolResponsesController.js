7803aba9f9341d99d6bd57b4548ad515faa79778
Adam Goldman <goldy@adamgoldman.me>
2018-05-10 14:35:48 +0200

    fetch tool responses in reverse order in admin
    resolves #105

--- src/server/api/toolResponses/toolResponsesController.js
+++ src/server/api/toolResponses/toolResponsesController.js
@@ -7,7 +7,9 @@ export default {
 }
 
 function getAll(req, res, next) {
-  ToolResponses.find().select('createdAt user title currentStepNum status')
+  ToolResponses.find()
+    .select('createdAt user title currentStepNum status')
+    .sort('-createdAt')
     .populate('user')
     .then(tools => res.json(tools))
     .catch(next)
