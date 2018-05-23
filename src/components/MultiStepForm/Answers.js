a2c8aa38f154643c574fec26f21daba1c50ce116
Adam Goldman <goldy@adamgoldman.me>
2018-05-23 09:36:35 +0100

    MultiStepForm fix link answers UI width

--- src/components/MultiStepForm/Answers.js
+++ src/components/MultiStepForm/Answers.js
@@ -32,13 +32,13 @@ class Answers extends React.Component {
         {answers.map((answer, idx) => {
           let html
           if (answer.isConcern) {
-            html = <ExternalA className="btn btn-primary" onClick={this.concernClick} href={MESSENGER_LINK_TOOL_CONCERN}>{answer.text}</ExternalA>
+            html = <ExternalA className="btn btn-primary btn-fixed" onClick={this.concernClick} href={MESSENGER_LINK_TOOL_CONCERN}>{answer.text}</ExternalA>
           } else if (answer.isFbShare) {
-            html = <ExternalA className="btn btn-primary" href={getFbShareUrl(path)}>{answer.text}</ExternalA>
+            html = <ExternalA className="btn btn-primary btn-fixed" href={getFbShareUrl(path)}>{answer.text}</ExternalA>
           } else if (answer.link) {
-            html = <Link className="btn btn-primary" to={answer.link}>{answer.text}</Link>
+            html = <Link className="btn btn-primary btn-fixed" to={answer.link}>{answer.text}</Link>
           } else if (answer.linkNew) {
-            html = <ExternalA className="btn btn-primary" href={answer.linkNew}>{answer.text}</ExternalA>
+            html = <ExternalA className="btn btn-primary btn-fixed" href={answer.linkNew}>{answer.text}</ExternalA>
           } else if (answer.isOther) {
             html = (
               <form onSubmit={(evt) => {
