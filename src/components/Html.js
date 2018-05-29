0c06b8d5983997573f3e84c1b823ed6dcd9e391a
Adam Goldman <goldy@adamgoldman.me>
2018-05-29 11:33:50 +0100

    add hotjar script

--- src/components/Html.js
+++ src/components/Html.js
@@ -104,6 +104,20 @@ class Html extends React.Component {
               dangerouslySetInnerHTML={{ __html: style.cssText }}
             />
           ))}
+          {/* Hotjar Tracking Code for https://www.adamgoldman.me/ */}
+          <script
+            dangerouslySetInnerHTML={{
+                __html:
+                    `(function hotjar(h, o, t, j, a, r) {
+                      h.hj = h.hj || function hotjarInner() { (h.hj.q = h.hj.q || []).push(arguments) }
+                      h._hjSettings = { hjid: 896952, hjsv: 6 }
+                      a = o.getElementsByTagName('head')[0]
+                      r = o.createElement('script'); r.async = 1
+                      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+                      a.appendChild(r)
+                    }(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv='))`,
+              }}
+          />
         </head>
         <body>
           <div id="fb-root" />
