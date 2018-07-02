8c917f74b67ee179f9e5382321e43529196c513e
Adam Goldman <goldy@adamgoldman.me>
2018-07-02 09:55:14 +0300

    (Savoring): improve payment step

--- src/server/api/toolResponses/savoringReviewSteps.js
+++ src/server/api/toolResponses/savoringReviewSteps.js
@@ -20,12 +20,20 @@ export default [
     description: `Great!
 I’m glad you’ve found value in our time together.
 
-Like I said in the beginning, I work with a [value first biz model](/savoring-your-child/pricingNEW), so you get to decide how much you want to pay back for the benefits you got from this process.`,
+Like I said in the beginning, I work with a [value first biz model](/savoring-your-child/pricingNEW), so you get to decide how much you want to pay back for the benefits you got from this process.
+
+> Thank you for all you are doing for all the parents of Helping Parents Heal.
+
+![Tywana Smith](https://res.cloudinary.com/goldylucks/image/upload/v1/users/tywana_m_smith)]
+
+&dash; Tywana Smith, moderator and facilitator leader for Helping Parents Heal online group.
+`,
+    notes: 'A portion of your support will go directly to helping parents heal non-profit organisation',
     type: 'radio',
     answers: [
-      { text: 'I got some good insights - $9', price: 9, goToStepById: 'payment' },
-      { text: 'Felt an emotional shift - $23', price: 23, goToStepById: 'payment' },
-      { text: 'This is wonderful, I feel great - $70', price: 70, goToStepById: 'payment' },
+      { text: 'I\'m grateful for your work - $9', price: 9, goToStepById: 'payment' },
+      { text: 'I\'m happy to gift this to the next parent - $23', price: 23, goToStepById: 'payment' },
+      { text: 'I want to support further development for  future parents - $70', price: 70, goToStepById: 'payment' },
       { text: 'I’m actually low on $$$ :(' },
       { text: 'Let me do another module before I pay' },
     ],
@@ -40,7 +48,7 @@ That’s perfectly fine. Our #1 priority is to reconnect you with what you value
 You are more than welcome to share this with others parents of course`,
     type: 'radio',
     answers: [
-      { text: 'You know what, I can spare $7', price: 7 },
+      { text: 'You know what, I can spare $7 to support future parents', price: 7 },
       { text: 'OK, let\'s continue', goToStepById: 'finalComments' },
       { text: 'Thank you for understanding Adam', goToStepById: 'finalComments' },
     ],
