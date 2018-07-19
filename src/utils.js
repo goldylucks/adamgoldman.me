6116dd81f02a526a72811296de6200bf768fb179
Adam Goldman <goldy@adamgoldman.me>
2018-07-19 20:33:20 +0300

    hotfix tools crash (scrollToTopOfNode)

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,7 @@ export const noop = () => null
 
 export const isProd = process.env.NODE_ENV === 'production'
 
-export const scrollToTopOfNode = (node, { duration = 300, topOffest = 0 }) => {
+export const scrollToTopOfNode = (node, { duration = 300, topOffest = 0 } = {}) => {
   const top = node.getBoundingClientRect().top - document.body.getBoundingClientRect().top
   scrollToElem(document.querySelector('html'), top - topOffest, duration)
 }
