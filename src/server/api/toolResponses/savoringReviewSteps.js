6ff7daa6a5263e15403f1e871770aad0402d869e
Adam Goldman <goldy@adamgoldman.me>
2018-09-18 23:27:09 -0300

    savoring remove value first from reviews

--- src/server/api/toolResponses/savoringReviewSteps.js
+++ src/server/api/toolResponses/savoringReviewSteps.js
@@ -4,7 +4,7 @@
 export default ({ toolSlug }) => [
   {
     title: 'Review - most useful',
-    description: '[aa9e2f6e92b54af285c729078aa6db39](LoomEmbedd)\n\nWhich part did you find the most useful, and why?\n (or found more beneficial/healing)',
+    description: 'Which part did you find the most useful, and why?\n (or found more beneficial/healing)',
     type: 'long',
     id: 'most-useful',
     gaEventAction: '100 Go to most useful',
@@ -12,72 +12,16 @@ export default ({ toolSlug }) => [
   },
   {
     title: 'Review - rate',
-    description: '[e62767011bd74d77aeb05c782fdf7717](LoomEmbedd)\n\nHow much did you find this process useful/beneficial/healing?',
+    description: 'nHow much did you find this process useful/beneficial/healing?',
     type: 'stars-review',
     gaEventAction: '110 most useful -> stars',
     hasValue: true,
   },
   {
-    title: 'Value first biz model - choose',
-    id: 'choosePaymentAmount',
-    gaEventAction: '120 stars -> payment intro',
-    description: `[eb68c28fb30b4e159b393078439f61e7](LoomEmbedd)
-
-Great!
-I’m glad you’ve found value in our time together.
-
-Like I said in the beginning, I work with a [value first biz model](/savoring-your-child/pricingNEW), so you get to decide how much you want to pay back for the benefits you got from this process.
-
-> Thank you for all you are doing for all the parents of Helping Parents Heal.
-
-![Tywana Smith](https://res.cloudinary.com/goldylucks/image/upload/v1/users/tywana_m_smith)]
-
-&dash; Tywana Smith, moderator and facilitator leader for Helping Parents Heal online group.
-`,
-    notes: 'A portion of your support will go directly to helping parents heal non-profit organisation',
-    type: 'radio',
-    answers: [
-      { text: 'I\'m grateful for your work - $9', price: 9, goToStepById: 'payment' },
-      { text: 'I\'m happy to gift this to the next parent - $23', price: 23, goToStepById: 'payment' },
-      { text: 'I want to support further development for  future parents - $70', price: 70, goToStepById: 'payment' },
-      { text: 'I’m actually low on $$$ :(' },
-      { text: 'Let me do another module before I pay' },
-    ],
-  },
-  {
-    title: 'Value first biz model',
-    id: 'low-on-cash',
-    gaEventAction: '131 Low on cash',
-    description: `\${echo}
-
-That’s perfectly fine. Our #1 priority is to reconnect you with what you value and appreciate in your relationship with \${hchildName}, give you more control over your internal experiences, emotions, and thought processes, hence increasing the freedom and choices in your life.
-
-You are more than welcome to share this with others parents of course`,
-    type: 'radio',
-    answers: [
-      { text: 'You know what, I can spare $7 to support future parents', price: 7 },
-      { text: 'OK, let\'s continue', goToStepById: 'finalComments' },
-      { text: 'Thank you for understanding Adam', goToStepById: 'finalComments' },
-    ],
-  },
-  {
-    title: 'Thank you for your support!',
-    id: 'payment',
-    gaEventAction: '132 Payment',
-    description: '[b843c523b7de4f5c8951b9bafdf48005](LoomEmbedd)\n\nThis allows me to invest more time to improve these processes and help more parents worldwide, just like you.',
-    type: 'payment',
-    answers: [
-      { text: 'Made the payment, let\'s continue' },
-      { text: 'Paid back as promised, thank you!' },
-    ],
-  },
-  {
     title: 'Final comments & suggestions',
     id: 'finalComments',
     gaEventAction: '140 Final Comments',
-    description: `[2e0ddfb8f31d4cfa98110c69d0868ad8](LoomEmbedd)
-
-Do you want to help me improve this process for the next parent?
+    description: `Do you want to help me improve this process for the next parent?
 
 Please share with me how was it for you. Any suggestions you might have? Any questions? Critics? Comments? Thoughts?
 
