e8d41034c228be89f16b6a508ef3f7645f6c8785
Adam Goldman <goldy@adamgoldman.me>
2018-11-20 11:49:52 -0500

    remove tracking: hotjar, adwords, facebok

--- src/components/Html.js
+++ src/components/Html.js
@@ -104,58 +104,6 @@ class Html extends React.Component {
               dangerouslySetInnerHTML={{ __html: style.cssText }}
             />
           ))}
-          {/* Hotjar Tracking Code for https://www.adamgoldman.me/ */}
-          <script
-            dangerouslySetInnerHTML={{
-                __html:
-                    `(function hotjar(h, o, t, j, a, r) {
-                      h.hj = h.hj || function hotjarInner() { (h.hj.q = h.hj.q || []).push(arguments) }
-                      h._hjSettings = { hjid: 896952, hjsv: 6 }
-                      a = o.getElementsByTagName('head')[0]
-                      r = o.createElement('script'); r.async = 1
-                      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-                      a.appendChild(r)
-                    }(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv='))`,
-              }}
-          />
-          {/* adwords conversion start */}
-          <script
-            src="https://www.googletagmanager.com/gtag/js?id=AW-842400866"
-            async
-            defer
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-              `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              
-              gtag('config', 'AW-842400866');`,
-            }}
-          />
-          {/* adwords conversion end */}
-
-          {/* FB pixel start */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-              `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                
-                fbq('init', '1153228348161182');
-                fbq('track', 'PageView');`,
-            }}
-          />
-          {/* FB pixel end */}
         </head>
         <body>
           <div id="fb-root" />
